7949b9064ddc444b3669eb1916e91bbdec66e80f
SimpleSol <gworkmaill@gmail.com>
2015-11-30 14:35:37 +0300

    commit

--- app/src/main/java/AppDelegate.java
+++ app/src/main/java/AppDelegate.java
@@ -1,7 +1,23 @@
+import android.app.AlarmManager;
 import android.app.Application;
+import android.app.PendingIntent;
+import android.content.Intent;
+
+import com.example.leon.rssreader2.service.SyncService;
 
 /**
  * Created by Leon on 18.11.2015.
  */
 public class AppDelegate extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        final AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
+        am.setRepeating(AlarmManager.RTC, 5000, 60000, PendingIntent
+                .getService(getApplicationContext(), 100500,
+                        new Intent(getApplicationContext(), SyncService.class),
+                        PendingIntent.FLAG_CANCEL_CURRENT));
+
+    }
 }
---
+++ app/src/main/java/com/example/leon/rssreader2/service/SyncService.java
@@ -0,0 +1,20 @@
+package com.example.leon.rssreader2.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by Leon on 30.11.2015.
+ */
+public class SyncService extends IntentService {
+
+    public SyncService() {
+        super("SyncService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.d("SyncService", "onHandleIntent");
+    }
+}
