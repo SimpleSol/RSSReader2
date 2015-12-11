da303f274293f0e739a126261ad30cdd938372c8
SimpleSol <gworkmaill@gmail.com>
2015-12-11 20:51:05 +0300

    final commit

---
+++ app/src/main/java/com/example/leon/rssreader2/AppDelegate.java
@@ -0,0 +1,7 @@
+package com.example.leon.rssreader2;
+
+/**
+ * Created by Leon on 04.12.2015.
+ */
+public class AppDelegate {
+}
---
+++ app/src/main/java/com/example/leon/rssreader2/app/NetworkWatcher.java
@@ -0,0 +1,7 @@
+package com.example.leon.rssreader2.app;
+
+/**
+ * Created by Leon on 04.12.2015.
+ */
+public class NetworkWatcher {
+}
